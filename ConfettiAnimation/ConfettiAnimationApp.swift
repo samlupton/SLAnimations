3798da4d6db2b4edadbf6e7bc1673b67af63be79
moo20222 <samuel@ou.edu>
2025-09-25 07:00:50 -0500

    Initial Commit

---
+++ ConfettiAnimation/ConfettiAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiAnimationApp.swift
+//  ConfettiAnimation
+//
+//  Created by Samuel Lupton on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConfettiAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConfettiAnimation
+//
+//  Created by Samuel Lupton on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
