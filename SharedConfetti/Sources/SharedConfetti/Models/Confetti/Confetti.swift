e9c968dfe5e9300a89689999f552a4491bca3b53
moo20222 <samuel@ou.edu>
2026-04-19 21:21:59 -0500

    Config

--- SharedConfetti/Sources/SharedConfetti/Models/Confetti/Confetti.swift
+++ SharedConfetti/Sources/SharedConfetti/Models/Confetti/Confetti.swift
@@ -1,5 +1,5 @@
 //
-//  ConfettiConfiguration.swift
+//  Confetti.swift
 //  SharedConfetti
 //
 //  Created by Samuel Lupton on 4/15/26.
@@ -7,33 +7,4 @@
 
 import UIKit
 
-public enum Confetti {
-    public struct Configuration: Sendable {
-        let cells: [Cell]
-    }
-}
-
-public extension Confetti.Configuration {
-    static let shower: Self = makeShower()
-    static let fountain: Self = makeShower()
-}
-
-// MARK: - Factory Methods
-
-extension Confetti.Configuration {
-    internal static func makeShower(
-        with contents: [Confetti.Cell.Content] = Confetti.Cell.Content.makeShowerContents()
-    ) -> Self {
-        return Confetti.Configuration(
-            cells: Confetti.Cell.makeShowerCells(with: contents)
-        )
-    }
-    
-    internal static func makeFountain(
-        with contents: [Confetti.Cell.Content] = Confetti.Cell.Content.makeFountainContents()
-    ) -> Self {
-        return Confetti.Configuration(
-            cells: Confetti.Cell.makeFountainCells(with: contents)
-        )
-    }
-}
+public enum Confetti {}
---
+++ SharedConfetti/Sources/SharedConfetti/Models/Confetti/Extensions/Confetti+Cell/Confetti+Configuration.swift
@@ -0,0 +1,37 @@
+//
+//  ConfettiConfiguration.swift
+//  SharedConfetti
+//
+//  Created by Samuel Lupton on 4/15/26.
+//
+
+public extension Confetti {
+    public struct Configuration: Sendable {
+        let cells: [Cell]
+    }
+}
+
+public extension Confetti.Configuration {
+    static let shower: Self = makeShower()
+    static let fountain: Self = makeFountain()
+}
+
+// MARK: - Factory Methods
+
+extension Confetti.Configuration {
+    internal static func makeShower(
+        with contents: [Confetti.Cell.Content] = Confetti.Cell.Content.makeShowerContents()
+    ) -> Self {
+        return Confetti.Configuration(
+            cells: Confetti.Cell.makeShowerCells(with: contents)
+        )
+    }
+    
+    internal static func makeFountain(
+        with contents: [Confetti.Cell.Content] = Confetti.Cell.Content.makeFountainContents()
+    ) -> Self {
+        return Confetti.Configuration(
+            cells: Confetti.Cell.makeFountainCells(with: contents)
+        )
+    }
+}
