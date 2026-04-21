ed2b6aa6b806f2321e06540b2687d8b2d5c241ef
moo20222 <samuel@ou.edu>
2026-04-20 20:05:29 -0500

    method name refactor

--- SharedConfetti/Sources/SharedConfetti/Protocols/ConfettiRenderer.swift
+++ SharedConfetti/Sources/SharedConfetti/Protocols/ConfettiRenderer.swift
@@ -16,13 +16,13 @@ internal protocol ConfettiRenderer {
     // MARK: - CAEmitterCell Set Up Methods
     
     func makeCACell(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeAcceleration(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeContent(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeEmission(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeScale(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeVelocity(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeLifetime(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
-    func makeSpin(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellAcceleration(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellContent(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellEmission(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellScale(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellVelocity(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellLifetime(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
+    func makeCellSpin(_ cacell: inout CAEmitterCell, cell: Confetti.Cell)
     
     // MARK: - CAEmitterLayer Set Up Methods
     
@@ -37,51 +37,51 @@ internal protocol ConfettiRenderer {
 
 extension ConfettiRenderer {
     func makeCACell(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
-        makeAcceleration(&cacell, cell: cell)
-        makeContent(&cacell, cell: cell)
-        makeEmission(&cacell, cell: cell)
-        makeScale(&cacell, cell: cell)
-        makeVelocity(&cacell, cell: cell)
-        makeLifetime(&cacell, cell: cell)
-        makeSpin(&cacell, cell: cell)
+        makeCellAcceleration(&cacell, cell: cell)
+        makeCellContent(&cacell, cell: cell)
+        makeCellEmission(&cacell, cell: cell)
+        makeCellScale(&cacell, cell: cell)
+        makeCellVelocity(&cacell, cell: cell)
+        makeCellLifetime(&cacell, cell: cell)
+        makeCellSpin(&cacell, cell: cell)
     }
     
-    func makeAcceleration(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellAcceleration(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.xAcceleration = cell.acceleration.x
         cacell.yAcceleration = cell.acceleration.y
         cacell.zAcceleration = cell.acceleration.z
     }
     
-    func makeContent(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellContent(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.scale = cell.contents.scale
         cacell.contents = cell.contents.image
         cacell.contentsRect = cell.contents.rect
     }
     
-    func makeEmission(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellEmission(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.emissionLongitude = cell.emission.longitude
         cacell.emissionLatitude = cell.emission.latitude
         cacell.emissionRange = cell.emission.range
     }
     
-    func makeScale(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellScale(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.scale = cell.scale.base
         cacell.scaleRange = cell.scale.range
         cacell.scaleSpeed = cell.scale.speed
     }
     
-    func makeVelocity(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellVelocity(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.velocity = cell.velocity.base
         cacell.velocityRange = cell.velocity.range
     }
     
-    func makeLifetime(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellLifetime(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.lifetime = cell.lifetime.base
         cacell.lifetimeRange = cell.lifetime.range
         cacell.birthRate = cell.lifetime.birthRate
     }
     
-    func makeSpin(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
+    func makeCellSpin(_ cacell: inout CAEmitterCell, cell: Confetti.Cell) {
         cacell.spin = cell.spin.base
         cacell.spinRange = cell.spin.range
     }
