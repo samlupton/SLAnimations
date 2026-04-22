155e6454d2fb8ae03067c0512e2270f8dca39b64
moo20222 <samuel@ou.edu>
2026-04-21 20:52:07 -0500

    Public type alias

---
+++ SharedConfetti/Sources/SharedConfetti/Typealiases/Typealiases.swift
@@ -0,0 +1,23 @@
+//
+//  Typealiases.swift
+//  SharedConfetti
+//
+//  Created by Samuel Lupton on 4/21/26.
+//
+
+public typealias ConfettiCell = Confetti.Cell
+public typealias ConfettiConfiguration = Confetti.Configuration
+public typealias ConfettiEmitter = Confetti.Emitter
+public typealias ConfettiStyle = Confetti.Style
+
+public typealias CellAcceleration = Confetti.Cell.Acceleration
+public typealias CellContents = Confetti.Cell.Contents
+public typealias CellEmission = Confetti.Cell.Emission
+public typealias CellLifetime = Confetti.Cell.Lifetime
+public typealias CellScale = Confetti.Cell.Scale
+public typealias CellSpin = Confetti.Cell.Spin
+public typealias CellVelocity = Confetti.Cell.Velocity
+
+public typealias EmitterGeometry = Confetti.Emitter.Geometry
+public typealias EmitterMode = Confetti.Emitter.Mode
+public typealias EmitterShape = Confetti.Emitter.Shape
