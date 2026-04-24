a80776a8488c75fa200b30968cc4ee2b230bfc1e
moo20222 <samuel@ou.edu>
2026-04-24 12:48:08 -0500

    Inits for different image types

--- Sources/Plume/Public/Models/Plume/Cell/Content.swift
+++ Sources/Plume/Public/Models/Plume/Cell/Content.swift
@@ -6,6 +6,8 @@
 //
 
 import CoreGraphics
+import UIKit
+import SwiftUI
 
 public extension Plume.Cell {
     struct Contents: Sendable {
@@ -14,11 +16,32 @@ public extension Plume.Cell {
         public var scale: CGFloat
         
         public init(
-            image: CGImage,
+            cgimage: CGImage,
             rect: CGRect = .init(x: 0, y: 0, width: 1, height: 1),
             scale: CGFloat = 1
         ) {
-            self.image = image
+            self.image = cgimage
+            self.rect = rect
+            self.scale = scale
+        }
+        
+        public init(
+            uiimage: UIImage,
+            rect: CGRect = .init(x: 0, y: 0, width: 1, height: 1),
+            scale: CGFloat = 1
+        ) {
+            self.image = uiimage.cgImage
+            self.rect = rect
+            self.scale = scale
+        }
+        
+        @available(iOS 17.0, *)
+        public init(
+            resource: ImageResource,
+            rect: CGRect = .init(x: 0, y: 0, width: 1, height: 1),
+            scale: CGFloat = 1
+        ) {
+            self.image = UIImage(resource: resource).cgImage
             self.rect = rect
             self.scale = scale
         }
