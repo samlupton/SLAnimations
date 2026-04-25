57dd927ee9a439254da2b2153048ec063e5a0ae4
moo20222 <samuel@ou.edu>
2026-04-25 18:29:54 -0500

    default cell velocity

--- Sources/Plume/Public/Models/Plume/Cell/Velocity.swift
+++ Sources/Plume/Public/Models/Plume/Cell/Velocity.swift
@@ -22,7 +22,7 @@ public extension Plume.Cell {
 public extension Plume.Cell.Velocity {
     static let none: Self = .init(base: 0)
     static let slow: Self = .init(base: 30, range: 10)
-    static let normal: Self = .init(base: 100, range: 30)
+    static let `default`: Self = .init(base: 100, range: 30)
     static let fast: Self = .init(base: 200, range: 50)
     static let burst: Self = .init(base: 400, range: 100)
 }
