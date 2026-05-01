8740dbf6065092aac9bb7e6c746eb872592e87b2
moo20222 <samuel@ou.edu>
2026-04-30 19:43:50 -0500

    plum init tests

--- Tests/PlumeTests/SharedPlumeTests.swift
+++ Tests/PlumeTests/SharedPlumeTests.swift
@@ -2,11 +2,51 @@ import XCTest
 @testable import Plume
 
 final class PlumeTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    var plume: Plume!
+    
+    override func setUp() {
+        self.plume = Plume(emitter: .circle(birthRate: .zero), cells: [])
+    }
+    
+    func test_plume_init() {
+        plume = Plume(emitter: .circle(birthRate: 10), cells: cells(count: .zero))
+        XCTAssertEqual(plume.emitter.mode, .surface)
+        XCTAssertEqual(plume.emitter.shape, .circle)
+        XCTAssertEqual(plume.emitter.birthRate, 10)
+        XCTAssertEqual(plume.cells.count, .zero)
+        
+        plume = Plume(emitter: .line(birthRate: 11), cells: cells(count: 1))
+        XCTAssertEqual(plume.emitter.mode, .outline)
+        XCTAssertEqual(plume.emitter.shape, .line)
+        XCTAssertEqual(plume.emitter.birthRate, 11)
+        XCTAssertEqual(plume.cells.count, 1)
+        
+        plume = Plume(emitter: .point(birthRate: 12), cells: cells(count: 2))
+        XCTAssertEqual(plume.emitter.mode, .points)
+        XCTAssertEqual(plume.emitter.shape, .point)
+        XCTAssertEqual(plume.emitter.birthRate, 12)
+        XCTAssertEqual(plume.cells.count, 2)
+        
+        plume = Plume(emitter: .rectangle(birthRate: 13), cells: cells(count: 3))
+        XCTAssertEqual(plume.emitter.mode, .surface)
+        XCTAssertEqual(plume.emitter.shape, .rectangle)
+        XCTAssertEqual(plume.emitter.birthRate, 13)
+        XCTAssertEqual(plume.cells.count, 3)
+    }
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+// MARK: - Testings Constants
+
+extension PlumeTests {
+    func cells(count: Int) -> [Plume.Cell] {
+        return .make(
+            from: Array(repeating: .actions, count: count),
+            lifetime: .ambient,
+            spin: .chaotic,
+            scale: .huge,
+            acceleration: .downLeft,
+            velocity: .explosive,
+            angle: .bottomHemisphere
+        )
     }
 }
